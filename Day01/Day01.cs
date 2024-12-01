1a5cbf93fae62a97e33148ef3072af7ac64c0168
Supernova <ldorval@gmail.com>
2024-12-01 14:54:47 -0500

    Day 1

--- Day01/Day01.cs
+++ Day01/Day01.cs
@@ -2,5 +2,16 @@
 
 public class Day01
 {
+    public static Day01Result Solve(List<string> input)
+    {
+        var splits = input.Select(x => x.Split("   "));
+        var firstList = splits.Select(x => int.Parse(x[0])).OrderBy(x => x).ToList();
+        var secondList = splits.Select(x => int.Parse(x[1])).OrderBy(x => x).ToList();
+
+        return new Day01Result(
+            firstList.Select((x, i) => Math.Abs(x - secondList[i])).Sum(),
+            firstList.Select(x => x * secondList.Count(y => y == x)).Sum());
+    }
     
+    public record Day01Result(long TotalDistance, long SimilarityScore);
 }
--- Day01/Day01Test.cs
+++ Day01/Day01Test.cs
@@ -1,6 +1,35 @@
-﻿namespace AdventOfCode2024.Day01;
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode2024.Day01;
 
 public class Day01Test
 {
+    [Test]
+    public void Day01Example()
+    {
+        var input = InputReader.ReadAll("Day01Example").AsListOfStrings();
+        Day01.Solve(input).TotalDistance.Should().Be(11);
+    }
+
+    [Test]
+    public void Day01Solution()
+    {
+        var input = InputReader.ReadAll("Day01").AsListOfStrings();
+        Console.WriteLine(Day01.Solve(input).TotalDistance);
+    }
+
+    [Test]
+    public void Day01Part2Example()
+    {
+        var input = InputReader.ReadAll("Day01Example").AsListOfStrings();
+        Day01.Solve(input).SimilarityScore.Should().Be(31);
+    }
     
+    [Test]
+    public void Day01Part2Solution()
+    {
+        var input = InputReader.ReadAll("Day01").AsListOfStrings();
+        Console.WriteLine(Day01.Solve(input).SimilarityScore);
+    }
 }
