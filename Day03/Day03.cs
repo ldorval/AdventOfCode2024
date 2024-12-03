03e13313ea7c60942554fc13d57386e6d156bf65
Luc Dorval <luc.dorval@fondaction.com>
2024-12-03 11:14:17 -0500

    Day 3

---
+++ Day03/Day03.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Day03;
+
+public class Day03
+{
+    public static int Solve(string input, bool enableDoStatements)
+    {
+        var matches = new Regex(@"(mul\((-?\d+),(-?\d+)\)|do\(\)|don't\(\))").Matches(input);
+        var sum = 0;
+        var enabled = true;
+        
+        foreach (Match match in matches)
+        {
+            if (match.Groups[0].Value.StartsWith("mul("))
+                sum += enabled ? int.Parse(match.Groups[2].Value) * int.Parse(match.Groups[3].Value) : 0;
+            else if (match.Groups[0].Value == "do()" && enableDoStatements)
+                enabled = true;
+            else if (match.Groups[0].Value == "don't()" && enableDoStatements)
+                enabled = false;
+        }
+        
+        return sum;
+    }
+}
---
+++ Day03/Day03Test.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode2024.Day03;
+
+public class Day03Test
+{
+    [Test]
+    public void Day03ExamplePart1()
+    {
+        Day03.Solve("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))", false).Should().Be(161);
+    }
+
+    [Test]
+    public void Day03Solution()
+    {
+        Console.WriteLine(Day03.Solve(InputReader.ReadAll("Day03"), false));
+    }
+
+    [Test]
+    public void Day03ExamplePart2()
+    {
+        Day03.Solve("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", true).Should().Be(48);
+    }
+
+    [Test]
+    public void Day03Part2Solution()
+    {
+        Console.WriteLine(Day03.Solve(InputReader.ReadAll("Day03"), true));
+    }
+}
