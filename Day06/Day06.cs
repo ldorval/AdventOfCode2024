08231ce949226335cbd29ed11877f95dc2c215f4
Supernova <ldorval@gmail.com>
2024-12-07 14:14:53 -0500

    Day 6, part 2 is not working yet

---
+++ Day06/Day06.cs
@@ -0,0 +1,80 @@
+﻿namespace AdventOfCode2024.Day06;
+
+public class Day06
+{
+    private static List<MapDirection> directions = new() { new(0, -1, '^'), new(1, 0, '>'), new(0, 1, 'v'), new(-1, 0, '<') };
+
+    public static int SolvePart1(string input)
+    {
+        var map = input.AsListOfStrings().Select((line, y) => line.Select((c, x) => new MapPosition(x, y, c)).ToList()).ToList();
+        return GetVisitedPositions(map).VisitedPositions.DistinctBy(x => x.Position).Count();
+    }
+
+    public static int SolvePart2(string input)
+    {
+        var map = input.AsListOfStrings().Select((line, y) => line.Select((c, x) => new MapPosition(x, y, c)).ToList()).ToList();
+        var initialResult = GetVisitedPositions(map);
+
+        var results = new List<Day06Result>();
+        foreach (var visitedPosition in initialResult.VisitedPositions)
+        {
+            if (visitedPosition.Position.Content != '#' && visitedPosition.Position.Content != '.') continue;
+            var position = visitedPosition.Position;
+            var alternateMap = map.ToList();
+            alternateMap[position.Y][position.X] = map[position.Y][position.X] with { Content = '#' };
+            results.Add(GetVisitedPositions(alternateMap));
+        }
+
+        return results.Count(x => x.IsLooping);
+    }
+
+    private static Day06Result GetVisitedPositions(List<List<MapPosition>> map)
+    {
+        var currentPosition = map.Select(position => position.SingleOrDefault(x => x.Content != '#' && x.Content != '.')).Single(x => x != null);
+        var currentDirection = directions.Single(direction => direction.Arrow == currentPosition.Content);
+        var visitedPositions = new List<VisitedPosition>();
+
+        while (true)
+        {
+            visitedPositions.Add(new VisitedPosition(currentPosition, currentDirection));
+
+            var destinationX = currentPosition.X + currentDirection.X;
+            var destinationY = currentPosition.Y + currentDirection.Y;
+            var destination = destinationY < map.Count && destinationY >= 0 && destinationX < map[0].Count && destinationX >= 0 ? map[destinationY][destinationX] : null;
+
+            if (currentPosition.Content is '#' or '.')
+            {
+                // if (visitedPositions.DistinctBy(x => x.Position.Content).Count() > 1)
+                //     return new Day06Result(visitedPositions, true);
+
+                if (destination == null) break;
+            }
+
+            if (destination.Content == '#')
+                currentDirection = TurnRight(currentDirection);
+            else
+                currentPosition = destination;
+        }
+
+        return new Day06Result(visitedPositions, false);
+    }
+
+    private static MapDirection TurnRight(MapDirection currentDirection)
+    {
+        return currentDirection.Arrow switch
+        {
+            '^' => directions.Single(direction => direction.Arrow == '>'),
+            '>' => directions.Single(direction => direction.Arrow == 'v'),
+            'v' => directions.Single(direction => direction.Arrow == '<'),
+            '<' => directions.Single(direction => direction.Arrow == '^')
+        };
+    }
+
+    private record MapPosition(int X, int Y, char Content);
+
+    private record MapDirection(int X, int Y, char Arrow);
+
+    private record Day06Result(List<VisitedPosition> VisitedPositions, bool IsLooping);
+
+    private record VisitedPosition(MapPosition Position, MapDirection Direction);
+}
---
+++ Day06/Day06Test.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode2024.Day06;
+
+public class Day06Test
+{
+    [Test]
+    public void ExamplePart1()
+    {
+        Day06.SolvePart1("Day06Example".ReadAll()).Should().Be(41);
+    }
+
+    [Test]
+    public void SolutionPart1()
+    {
+        Console.WriteLine(Day06.SolvePart1("Day06".ReadAll()));
+    }
+
+    [Test]
+    public void ExamplePart2()
+    {
+        Day06.SolvePart2("Day06Example".ReadAll()).Should().Be(6);
+    }
+}
